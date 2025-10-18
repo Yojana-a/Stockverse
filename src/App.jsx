9d0ecdbe9a9b245b18efe99582d0acc2b5caddf4
sandhyaneupane <dhyaneupane@gmail.com>
2025-10-18 18:01:29 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,26 @@ import BuySell from './pages/BuySell';
 import Account from './pages/Account';
 import Settings from './pages/Settings';
 import { DataProvider } from './context/DataContext';
-import { StockProvider } from './context/StockContext';
 import './App.css';
 
 function App() {
   return (
-    <StockProvider>
-      <DataProvider>
-        <Router>
-          <div className="min-h-screen bg-gray-50">
-            <Navbar />
-            <main className="container mx-auto px-4 py-8">
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/buy-sell" element={<BuySell />} />
-                <Route path="/account" element={<Account />} />
-                <Route path="/settings" element={<Settings />} />
-              </Routes>
-            </main>
-          </div>
-        </Router>
-      </DataProvider>
-    </StockProvider>
+    <DataProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/buy-sell" element={<BuySell />} />
+              <Route path="/account" element={<Account />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </DataProvider>
   );
 }
 
